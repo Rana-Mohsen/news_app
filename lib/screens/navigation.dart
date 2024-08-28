d2656ebd116d2d9788c6075d9405d13a363c6d23
Rana-3r2m <rana3r2m@gmail.com>
2024-08-28 23:15:22 +0300

    create nav bar

---
+++ lib/screens/navigation.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class Navigation extends StatefulWidget {
+  const Navigation({super.key});
+
+  @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  List<String> icons = [
+    'assets/icons/Home.png',
+    'assets/icons/Favorite.png',
+    'assets/icons/Profile.png'
+  ];
+  List<String> selectedIcons = [
+    'assets/icons/HomeActive.png',
+    'assets/icons/FavoriteActive.png',
+    'assets/icons/ProfileActive.png'
+  ];
+  List<String> titles = ['Home', 'Favorite', 'Profile'];
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(),
+          Align(
+            child: _navbar(),
+            alignment: Alignment.bottomCenter,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _navbar() {
+    return Container(
+      height: 65,
+      margin: const EdgeInsets.only(
+        right: 24,
+        left: 24,
+        bottom: 24,
+      ),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(32),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: Colors.black.withAlpha(20),
+              spreadRadius: 10,
+            )
+          ]),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: icons.map((icon) {
+          int index = icons.indexOf(icon);
+          bool isSelected = selectedIndex == index;
+          return Material(
+            color: Colors.transparent,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      margin: const EdgeInsets.only(
+                        top: 15,
+                        bottom: 0,
+                        left: 35,
+                        right: 35,
+                      ),
+                      child:
+                          Image.asset(isSelected ? selectedIcons[index] : icon),
+                    ),
+                    Text(
+                      titles[index],
+                      style: TextStyle(
+                        color: isSelected ? Colors.black : Colors.grey,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
