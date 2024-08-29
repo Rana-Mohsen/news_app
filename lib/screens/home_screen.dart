a4cc858c78158c02503b9d5536b5e6d80ffaca8c
Rana-3r2m <rana3r2m@gmail.com>
2024-08-29 22:01:43 +0300

    slider + listView function

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/constants.dart';
 import 'package:news_app/screens/widgets/custome_textfield.dart';
@@ -10,6 +11,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _sliderIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     var topHeight = MediaQuery.of(context).viewPadding.top;
@@ -33,10 +36,30 @@ class _HomeScreenState extends State<HomeScreen> {
                   color: Colors.white,
                 ),
               ),
+              _slider(),
             ],
           )
         ],
       ),
+    );
+  }
+
+  Widget _slider() {
+    return CarouselSlider.builder(
+      itemCount: 5,
+      itemBuilder: (BuildContext context, int itemIndex, int pageViewIndex) {
+        return Image.network("https://media.wired.com/photos/66ab594d0c0cc4819f595db4/191:100/w_1280,c_limit/073024_Crypto%20get%20rich%20quick.jpg");
+      },
+      options: CarouselOptions(
+          autoPlay: true,
+          enlargeCenterPage: true,
+          viewportFraction: 0.9,
+          aspectRatio: 2.0,
+          initialPage: 0,
+          onPageChanged: (index, reason) {
+            _sliderIndex = index;
+            setState(() {});
+          }),
     );
   }
 }
